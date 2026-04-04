3063e26e73f8f316758262fcd7da99c3a0449bd9
David <spro80@gmail.com>
2026-04-04 01:51:20 -0300

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,15 +18,6 @@ const socials = [
     ),
   },
   {
-    label: 'Twitter / X',
-    href: 'https://x.com/tu-usuario',
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.747l7.73-8.835L1.254 2.25H8.08l4.258 5.639 5.906-5.639zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-      </svg>
-    ),
-  },
-  {
     label: 'WhatsApp',
     href: 'https://wa.me/56989194282',
     icon: (
